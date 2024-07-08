0c5e8b36b256ad1c80da4b95b7a8a58485733833
Igor <makarov.igor.717@gmail.com>
2024-07-08 22:11:54 +1000

    update

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
+
+const { Telegraf } = require("telegraf");
+const axios = require("axios");
 const express = require("express");
 const crypto = require("crypto");
-const https = require("https");
 
+const bot = new Telegraf(process.env.BOT_TOKEN);
 const app = express();
 app.use(express.json());
 
 app.use(express.static("public"));
+
+const cors = require("cors");
+var corsOptions = {
+  origin: "https://boostlify.netlify.app",
+  optionsSuccessStatus: 200,
+  methods: "GET, POST",
+};
+app.use(cors(corsOptions));
 
 app.post("/api/telegram/validate", (request, response) => {
   const initData = new URLSearchParams(request.body.initData);
@@ -28,61 +40,26 @@ app.post("/api/telegram/validate", (request, response) => {
     .digest("hex");
 
   if (hash === _hash) {
-    response.sendStatus(200);
+    response.status(200).send("valid");
   } else {
-    response.sendStatus(401);
+    response.status(401).send("invalid");
   }
 });
 
 app.post("/api/telegram/userPhoto", async (request, response) => {
-  const file_id = await fetch(
-    "https://api.telegram.org/bot6498389353:AAErk3KqUxzPJPxwUBrv73b6MoDwbK0CEtQ/getUserProfilePhotos",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ user_id: request.body.id, limit: "1" }),
-    }
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      return response.result.photos[0][0].file_id;
-    });
+  const userId = request.body.id;
+  const photos = await bot.telegram.getUserProfilePhotos(userId, 0, 1);
+  const fileId = photos.photos[0][0].file_id;
+  const file = await bot.telegram.getFile(fileId);
+  const photoUrl = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${file.file_path}`;
 
-  const file_path = await fetch(
-    "https://api.telegram.org/bot6498389353:AAErk3KqUxzPJPxwUBrv73b6MoDwbK0CEtQ/getFile",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ file_id: file_id }),
-    }
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      return response.result.file_path;
-    });
-
-  https
-    .get(
-      `https://api.telegram.org/file/bot6498389353:AAErk3KqUxzPJPxwUBrv73b6MoDwbK0CEtQ/${file_path}`,
-      (resp) => {
-        let data = "";
-
-        resp.on("data", (chunk) => {
-          data += chunk;
-        });
-
-        resp.on("end", () => {
-          response.status(200).send(data);
-        });
-      }
-    )
-    .on("error", (err) => {
-      response.status(403);
-    });
+  const responseTelegram = await axios.get(photoUrl, {
+    responseType: "arraybuffer",
+  });
+  const base64String = Buffer.from(responseTelegram.data, "binary").toString(
+    "base64"
+  );
+  response.status(200).send(`data:image/jpeg;base64,${base64String}`);
 });
 
 app.listen(3030);
